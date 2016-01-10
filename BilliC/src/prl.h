62a669d11d3f5b4d62750f1357e8974f6cae3af5
Parham Alvani <parham.alvani@gmail.com>
2016-01-10 15:18:37 +0330

    ReadLine support was added.

--- BilliC/src/prl.h
+++ BilliC/src/prl.h
@@ -1,7 +1,7 @@
 /*
  * In The Name Of God
  * ========================================
- * [] File Name : main.c
+ * [] File Name : prl.h
  *
  * [] Creation Date : 10-01-2016
  *
@@ -12,5 +12,13 @@
  * Copyright (c) 2016 Parham Alvani.
 */
 
-int main(int argc, char *argv[]) {
-}
+#ifndef PRL_H
+#define PRL_H
+
+void prl_set_prompt(const char *p);
+
+const char *prl_get_prompt(void);
+
+char *prl_gets(void);
+
+#endif
