641fb9a07c1bcca7290bd6dfa906d808aafcfe6c
Parham Alvani <parham.alvani@gmail.com>
2016-01-10 14:50:09 +0330

    BilliC project structure was added.

---
+++ BilliC/src/main.c
@@ -0,0 +1,13 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : main.c
+ *
+ * [] Creation Date : 10-01-2016
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+/*
+ * Copyright (c) 2016 Parham Alvani.
+*/
