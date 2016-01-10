c645e5814bedaf0df4129e22ebd7cfba5eb33fe3
Parham Alvani <parham.alvani@gmail.com>
2016-01-10 19:17:29 +0330

    SQL scripts were added.

---
+++ BilliC/sql/init-user.sql
@@ -0,0 +1,22 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : init-2.sql
+ *
+ * [] Creation Date : 10-01-2016
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+/*
+ * Copyright (c) 2016 Parham Alvani.
+*/
+CREATE TABLE users (
+	s_id integer,
+	name varchar(255),
+	family varchar(255),
+	hometown varchar(255),
+	career varchar(255),
+	birthday date,
+	PRIMARY KEY (s_id)
+);
