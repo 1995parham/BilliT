1bbf896e3ac3f209bcd5d326013913cac9715f8f
Parham Alvani <parham.alvani@gmail.com>
2016-01-10 21:36:35 +0330

    Main was improved.

--- BilliC/src/main.c
+++ BilliC/src/main.c
@@ -11,6 +11,30 @@
 /*
  * Copyright (c) 2016 Parham Alvani.
 */
+#include <stdio.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
-int main(int argc, char *argv[]) {
+#include "command.h"
+#include "prl.h"
+
+int main(int argc, char *argv[])
+{
+	printf("BilliC version 0.2\n");
+	printf("Copyright (C) 2016 Parham Alvani (parham.alvani@gmail.com)\n");
+	printf("TFAT comes with ABSOLUTELY NO WARRANTY; for details type `show w'.\n");
+	printf("This is free software, and you are welcome to redistribute it\n");
+	printf("under certain conditions; type `show c' for details.\n");
+	printf("\n");
+
+	const char *command;
+	char prompt[1024];
+
+	while (true) {
+		sprintf(prompt, "@BilliC {} [] $ ");
+		prl_set_prompt(prompt);
+		command = prl_gets();
+		command_dispatcher(command);
+	}
 }
