abac4d28cea7b1a6b460e0b43209fddcc69cff9e
Parham Alvani <parham.alvani@gmail.com>
2016-01-11 17:30:36 +0330

    User table initiation was improved.

--- BilliC/sql/init-user.sql
+++ BilliC/sql/init-user.sql
@@ -13,10 +13,12 @@
 */
 CREATE TABLE users (
 	s_id integer,
+	username varchar(255),
 	name varchar(255),
 	family varchar(255),
 	hometown varchar(255),
 	career varchar(255),
 	birthday date,
 	PRIMARY KEY (s_id)
+	UNIQUE (username)
 );
