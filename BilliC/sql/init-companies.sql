041067429ee5f919c2b408dfa3384f03a7d751b4
Parham Alvani <parham.alvani@gmail.com>
2016-01-12 00:25:40 +0330

    Companies table initiation was improved.

--- BilliC/sql/init-companies.sql
+++ BilliC/sql/init-companies.sql
@@ -22,5 +22,8 @@ CREATE TABLE IF NOT EXISTS companies (
 	id integer not null primary key default nextval('companies_id_seq'),
 	manager integer not null references users(s_id),
 	name varchar(255) not null
+	driver integer not null CHECK (driver > 4),
+	pilot integer not null CHECK (pilot > 6),
+	bus integer not null CHECK (bus > 5)
 );
 
